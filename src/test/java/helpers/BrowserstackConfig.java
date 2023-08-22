7b61c5631ceb153426086a2ec3ef72cf7cfea20c
mikhailrezchikov <mrezchikov529@gmail.com>
2023-08-22 22:22:07 +0300

    fix

--- src/test/java/helpers/BrowserstackConfig.java
+++ src/test/java/helpers/BrowserstackConfig.java
@@ -20,7 +20,7 @@ public interface BrowserstackConfig extends Config {
     String getBaseUrl();
 
     @Key("app")
-    @DefaultValue("bs://sample.app")
+    @DefaultValue("bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c")
     String getApp();
 
     @Key("device")
